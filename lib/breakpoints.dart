288f734eddb9fbf226afec56310647e140d23e78
lydialausf <stam_pin@hotmail.com>
2024-12-22 13:23:17 +0800

    add responsivelayout

---
+++ lib/breakpoints.dart
@@ -0,0 +1,11 @@
+/// Namespace for Default WikiCamps Breakpoints
+abstract class MyPortfolioBreakpoints {
+  /// Max width for a small layout.
+  static const double small = 905;
+
+  /// Max width for a medium layout.
+  static const double medium = 1440;
+
+  /// Max width for a large layout.
+  static const double large = 1920;
+}
